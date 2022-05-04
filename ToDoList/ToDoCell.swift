1d3129c80f061dcbde5c611cde82de32f360c5be
Gaji Mahamud <gmm182@scarletmail.rutgers.edu>
2022-05-03 21:31:51 -0400

    Initial Commit

---
+++ ToDoList/ToDoCell.swift
@@ -0,0 +1,24 @@
+//
+//  ToDoCell.swift
+//  ToDoList
+//
+//  Created by Gaji Mahamud on 5/3/22.
+//
+
+import UIKit
+
+protocol ToDoCellDelegate {
+    func checkMarkTapped(_ sender: ToDoCell)
+}
+
+class ToDoCell: UITableViewCell {
+    
+    @IBOutlet weak var isCompleteButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    var delegate: ToDoCellDelegate?
+    
+    @IBAction func isCompleteButtonTapped(_ sender: UIButton) {
+        delegate?.checkMarkTapped(self)
+    }
+}
